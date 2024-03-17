fd8dbcbf0f0fe8adb06874f1780e8a87e375f270
Maxime Turcotte <maxime.turcotte@erudit.org>
2024-03-16 22:10:53 -0400

    Enable dap.core, lang.python, test.core & lazyrc

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,8 +14,13 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
+    -- the lazyrc extra must be the last plugin spec added
+    { import = "lazyvim.plugins.extras.lazyrc" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
